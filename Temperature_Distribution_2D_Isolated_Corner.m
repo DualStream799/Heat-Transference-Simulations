7fcc1c1653768da372766ab5e4f7a8b699e412f4
Arthur Alegro de Oliveira <dualstream799@gmail.com>
2021-10-04 14:40:45 -0300

    New simulation added

--- Temperature_Distribution_2D_Isolated_Corner.m
+++ Temperature_Distribution_2D_Isolated_Corner.m
@@ -5,7 +5,7 @@ t_range = 10; % s
 % Step intervals:
 dx = 0.1;      % m
 dy = 0.1;      % m
-dt = 0.01;     % s
+dt = 0.001;     % s
 % Thermic coefficient:
 k = 0.23;  % W/mm.ºC
 % Specific heat:
@@ -25,33 +25,36 @@ t_array = linspace(0, t_range, t_nodes);
 % Temperature array:
 T = zeros(x_nodes, y_nodes, t_nodes);
 % Initial temperature conditions:
-T(:, y_nodes, :) = 50;
 T(1,:,:) = 150;
+T(:,y_nodes,:) = 50;
 
 % Iteration to calculate the temperature distribution:
 for t = 2:t_nodes
      
     for j = 2:y_nodes-1
-        % Calculate the Temperature on the internal node:
+        
         for i = 1:x_nodes-1
+            % Calculate the Temperature on the isolated corner's nodes:
             if (i == 1)
                 dTdx = 0;
                 % Calculate the Central Diference on the X-axis:
-                d2Tdx2 = ( T(i+1,j,t-1) - 2*T(i,j,t-1) + ( T(i+1,j,t-1) - 2*dx*dTdx ) )/ dx^2;
+                d2Tdx2 = ( T(j,i+1,t-1) - 2*T(j,i,t-1) + T(j,i,t-1) - 2*dt*dTdx )/dx^2;
                 % Calculate the Central Diference on the Y-axis:
-                d2Tdy2 = ( T(i,j+1,t-1) - 2*T(i,j,t-1) + T(i,j-1,t-1) )/ dy^2;
-                % Calculate the Temperature variation on the next node:
-                dTdt = alpha*dt *( d2Tdx2 + d2Tdy2 ) + T(i,j,t-1);
-                % Calculate the Temperature on the next node:
-                T(i,j,t) = dTdt;
+                d2Tdy2 = ( T(j+1,i,t-1) - 2*T(j,i,t-1) + T(j-1,i,t-1) )/dy^2;
+                % Calculate the Temperature variation on the node:
+                dTdt = alpha*dt*( d2Tdx2 + d2Tdy2);
+                % Calculate the current Temperature on the node:
+                T(j,i,t) = dTdt + T(j,i,t-1);
+            % Calculate the Temperature on the internal nodes:
             else
                 % Calculate the Central Diference on the X-axis:
                 d2Tdx2 = ( T(i+1,j,t-1) - 2*T(i,j,t-1) + T(i-1,j,t-1) )/ dx^2;
                 % Calculate the Central Diference on the Y-axis:
                 d2Tdy2 = ( T(i,j+1,t-1) - 2*T(i,j,t-1) + T(i,j-1,t-1) )/ dy^2;
-                % Calculate the Temperature on the next node:
-                dTdt = alpha*dt*( d2Tdx2 + d2Tdy2 ) + T(i,j,t-1);
-                T(i,j,t) = dTdt;
+                % Calculate the Temperature variation on the node:
+                dTdt = alpha*dt*( d2Tdx2 + d2Tdy2 );
+                % Calculate the current Temperature on the node:
+                T(i,j,t) = dTdt + T(i,j,t-1);
             end
         end
     end
